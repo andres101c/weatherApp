61fe22e21d9ee39ec86f75e6c1717dc92cb6f465
andres101c <157847300+andres101c@users.noreply.github.com>
2024-04-30 23:08:58 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,82 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, TextInput, Button } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AuthScreen from './screens/AuthScreen';
+import AuthenticatedScreen from './screens/AuthenticatedScreen';
 
-// andres ibanez api call
+const Stack = createStackNavigator();
 
-const API_KEY = '8158ea1b56e2497492b163654241204'; 
-
-const App = () => {
-  const [weatherData, setWeatherData] = useState(null);
-  const [zipCode, setZipCode] = useState(''); 
-
-  const fetchWeatherData = () => {
-   
-
-    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${zipCode}&days=7`)
-   
-
-      .then(response => response.json())
-      .then(data => {
-        console.log('Weather Data:', data);
-        setWeatherData(data);
-      })
-      .catch(error => {
-        console.log('Error fetching weather data:', error);
-      });
-  };
-
-  useEffect(() => {
-    fetchWeatherData();
-  }, []);
-
+export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Enter Zip Code:</Text>
-      <TextInput
-        style={styles.input}
-        value={zipCode}
-        onChangeText={(text)=> setZipCode(text)}
-        placeholder="Enter zip code"
-        keyboardType="numeric"
-      />
-      <Button
-        title="Get Weather"
-        onPress={fetchWeatherData}
-      />
-      <StatusBar style="auto" />
-     
-      {weatherData && weatherData.current && (
-  <View style={styles.weatherContainer}>
-    <Text>Current Weather:</Text>
-    <Text>Temperature: {weatherData.current.temp_f}</Text>
-    <Text>Condition: {weatherData.current.condition.text}</Text>
-    <Text>time: {weatherData.location.localtime}</Text>
-  </View>
-)}
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="AuthScreen">
+        <Stack.Screen name="AuthScreen" component={AuthScreen} options={{ title: 'Sign In/Up' }} />
+        <Stack.Screen name="AuthenticatedScreen" component={AuthenticatedScreen} options={{ title: 'Welcome' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    height: 40,
-    width: '80%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-  },
-  weatherContainer: {
-    marginTop: 20,
-    alignItems: 'center',
-  },
-});
-
-export default App;
